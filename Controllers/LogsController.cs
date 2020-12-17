ef97cfb0ad04601ee0c619b565cd1b95149e2a75
Sahil Rajarai <sahil-007@live.com>
2020-12-17 02:02:04 +0000

    added logger implementation

---
+++ Controllers/LogsController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using McSharesAPI.Repository;
+using McSharesAPI.Models;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using McSharesAPI.Services;
+using Microsoft.AspNetCore.Http;
+
+namespace McSharesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class LogsController : ControllerBase
+    {
+        private ILoggerRepository _logger;
+        public LogsController(ILoggerRepository logger)
+        {
+            _logger = logger;
+        }
+
+         // GET: api/Customer/details
+        [HttpGet]
+        public List<Log> GetAll() =>
+            _logger.GetAllLogs();
+
+    }
+}
